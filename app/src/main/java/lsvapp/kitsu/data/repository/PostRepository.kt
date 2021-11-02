552d04dc523e006fd77278ce5cc06f93d9b6b802
lesiv <lesiv@myservices.digital>
2021-11-02 17:14:20 +0300

    add cache

--- app/src/main/java/lsvapp/kitsu/data/repository/PostRepository.kt
+++ app/src/main/java/lsvapp/kitsu/data/repository/PostRepository.kt
@@ -1,12 +1,30 @@
 package lsvapp.kitsu.data.repository
 
+import com.dropbox.android.external.store4.*
 import lsvapp.kitsu.data.network.kitsuAPI.PostApi
+import lsvapp.kitsu.domain.entity.PageContent
+import lsvapp.kitsu.domain.entity.dto.PostDto
 
 class PostRepository(
     private val postApi: PostApi
 ) {
-    suspend fun getPosts() = postApi.getPosts()
+
+    private val postStore: Store<PostQuery, PageContent<PostDto>> = StoreBuilder
+        .from(
+            Fetcher.of<PostQuery, PageContent<PostDto>> {
+                postApi.getPosts()
+            }
+        )
+        .build()
+
+    suspend fun getPosts() = postStore.get(PostQuery())
 
     suspend fun getAuthorPostUser(id: Long) = postApi.getAuthorPostUser(id)
 
 }
+
+data class PostQuery(
+    val page: Int? = null,
+    val size: Int? = null,
+    val id: Long? = null
+)
